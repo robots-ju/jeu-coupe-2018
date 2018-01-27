7bc5929661ac6a01c1458ac8825018cc588e5f93
bluedrack28 <bluedrack28@gmail.com>
2018-01-27 14:07:33 +0100

    Repartation de bug pour les deux liste.

--- web/Interface/game/src/components/Interface.vue
+++ web/Interface/game/src/components/Interface.vue
@@ -3,13 +3,13 @@
         <h1 class='title'>{{ pseudo.toUpperCase() }}</h1>
         <div class='middle'>
             <span class='start'>start</span>
-            <draggable v-model="liste" class="dragArea1" :options="{animation: 300,group:'people'}">
+            <draggable v-model="liste" class="dragArea1" :options="{animation: 300,group:{name:'block',pull:false}}">
                 <div class='block' v-for="element in liste" v-bind:key="element">{{element.name}}</div>
             </draggable>
         </div>
         <div class='bottom'>
-            <draggable v-model="blockStart" class="dragArea" :options="{animation: 300,sort:false,group:{ name:'people',  pull:'clone', put:true}}">
-                <div class='block' v-for="element in blockStart" v-bind:key="element" ><span class='center'>{{element.name}}</span></div>
+            <draggable v-model="blockStart" class="dragArea" :options="{animation: 300,sort:false,group:{ name:'block',  pull:'clone', put:true}}">
+                <div class='block' v-for="element in blockStart" v-bind:key="element" ><span v-bind:class='element.name'>{{element.name}}</span></div>
             </draggable>
         </div>
   </div>
@@ -30,18 +30,15 @@ export default {
             pseudo: 'bluedrack',
 
             blockStart: [
-                {id: 0, name:'avancer'},
-                {id: 1, name:'reculer'},
-                {id: 2, name:'tourner gauche'},
-                {id: 3, name:'tourner droite'},
-                {id: 4, name:'saisir'},
-                {id: 5, name:'lacher'},
+                {id: 0, class: 'avancer', name:'avancer'},
+                {id: 1, class: 'reculer', name:'reculer'},
+                {id: 2, class: 'tournerG', name:'tourner gauche'},
+                {id: 3, class: 'tournerD', name:'tourner droite'},
+                {id: 4, class: 'saisir', name:'saisir'},
+                {id: 5, class: 'lacher', name:'lacher'},
             ],
             liste: [],
-
-
             parentMsg: 10,
-
         }
     }
 }
@@ -61,7 +58,7 @@ body {
     font-family: Roboto;
 }
 .bottom {
-        display: inline-block;
+    display: inline-block;
 
     background-color: tomato;
     position: fixed;
@@ -76,13 +73,13 @@ body {
     background-color: aquamarine
 }
 .block {
+    background: white url(../assets/ouvrir.png) no-repeat center;
     display: inline-block;
     position: relative;
     vertical-align: middle;
     height: 90px;
     width: 100px;
     margin: 20px;
-    border: solid white 10px;
     background-color: azure;
     color: black;
     border-radius: 5%;
@@ -108,6 +105,7 @@ body {
     background-color: blue;
 }
 .dragArea1 {
+    text-align: center;
     display: inline;
     padding: 10px;
     background-color: blue;
