cee899cfd2f9b5df1adc7eb7620d14ac74b75d03
MorganArthox <morgan.chetelat@outlook.com>
2018-02-03 16:17:53 +0100

    affiche du nom signé le bonobos du Zimbabwe.... Il fait chaud

--- web/Interface/Accueil/script.js
+++ web/Interface/Accueil/script.js
@@ -1,5 +1,6 @@
 document.getElementById('commencer').addEventListener('click', () => {
-
+    let nom = document.getElementById('nom').value;
+    localStorage.setItem('nom',nom);
     window.open('../../simple/index.html');
 });
 document.addEventListener('DOMContentLoaded', () => {
