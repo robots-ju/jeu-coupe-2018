9ac688f563295a76cccce271aed48c1a20afe388
dani2000 <daniel.renaud@divcom.ch>
2018-03-10 14:22:00 +0100

    Modifications sur la page d'accueil

--- web/Interface/Accueil/script.js
+++ web/Interface/Accueil/script.js
@@ -1,5 +1,11 @@
-document.getElementById('commencer').addEventListener('click', () => {
+document.getElementById('commencer_simple').addEventListener('click', () => {
     let nom = document.getElementById('nom').value;
     localStorage.setItem('nom',nom);
     window.open('../../simple/index.html');
+});
+
+document.getElementById('commencer_autre').addEventListener('click', () => {
+    let nom = document.getElementById('nom').value;
+    localStorage.setItem('nom',nom);
+    window.open('../../Interface/game/index.html');
 });
